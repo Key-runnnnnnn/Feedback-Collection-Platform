f235185558427ad4edffb71bf14605aae8d06940
Kiran Dhawan <134703701+Key-runnnnnnn@users.noreply.github.com>
2025-07-19 22:04:07 +0530

    Update api.js

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1';
+const API_URL = 'https://feedback-collection-platform-xi.vercel.app/api/v1';
 
 // Create axios instance with credentials
 const api = axios.create({
