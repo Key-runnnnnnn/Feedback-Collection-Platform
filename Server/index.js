960d6a199e916fb0c9e1d885c4ff37c76bdfee8c
kiran <kiran.s.dhawan@gmail.com>
2025-07-20 12:18:30 +0530

    Refactor CORS middleware configuration for simplicity

--- Server/index.js
+++ Server/index.js
@@ -13,10 +13,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-}));
+
+
+app.use(cors());
+
+
 app.use(bodyParser.json());
 app.use(cookieParser());    
 app.get("/", (req, res) => {
